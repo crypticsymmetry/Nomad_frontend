0424f79e7e55052cd67ddc946b7010398849a312
crypticsymmetry <ccbb121@gmail.com>
2024-07-05 23:02:44 -0700

    Update MachineForm.js add worker

--- src/components/MachineForm.js
+++ src/components/MachineForm.js
@@ -4,11 +4,12 @@ import { useHistory } from 'react-router-dom';
 
 const MachineForm = () => {
     const [name, setName] = useState('');
+    const [workerName, setWorkerName] = useState('');
     const history = useHistory();
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        axios.post('/machines', { name })
+        axios.post('/machines', { name, worker_name: workerName })
             .then(response => {
                 history.push('/');
             })
@@ -23,6 +24,12 @@ const MachineForm = () => {
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 placeholder="Machine Name"
+            />
+            <input
+                type="text"
+                value={workerName}
+                onChange={(e) => setWorkerName(e.target.value)}
+                placeholder="Worker Name"
             />
             <button type="submit">Add Machine</button>
         </form>
