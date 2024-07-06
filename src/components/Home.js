4d9b00480986bce9a06e6ad5e2f1963384f1007b
crypticsymmetry <ccbb121@gmail.com>
2024-07-05 21:32:14 -0700

    Add files via upload

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
+import axios from './axiosConfig'; // Use the configured Axios instance
 import './Home.css';
 
 const Home = () => {
--- src/components/MachineDetail.js
+++ src/components/MachineDetail.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
 import Select from 'react-select';
+import axios from './axiosConfig'; // Use the configured Axios instance
 import './MachineDetail.css';
 
 const MachineDetail = () => {
