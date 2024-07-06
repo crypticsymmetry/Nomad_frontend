f67aa05a967b920d0371787513a2cbbed85466b9
crypticsymmetry <ccbb121@gmail.com>
2024-07-05 22:04:37 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import axios from 'axios';
+
+// Set the base URL for Axios requests
+axios.defaults.baseURL = 'https://nomad-backend-1.onrender.com'; // Replace with your Render backend URL
 
 ReactDOM.render(<App />, document.getElementById('root'));
