0b59c0e11d3f0d9a7e2428f919078ec5c453f116
crypticsymmetry <ccbb121@gmail.com>
2024-07-05 22:51:36 -0700

    Update MachineDetail.js

--- src/components/MachineDetail.js
+++ src/components/MachineDetail.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
 import Select from 'react-select';
-import axios from './axiosConfig'; // Use the configured Axios instance
 import './MachineDetail.css';
 
 const MachineDetail = () => {
@@ -147,6 +147,7 @@ const MachineDetail = () => {
                 ))}
             </ul>
             <input type="file" onChange={uploadPhoto} />
+            {machine.photo && <img src={`/images/${machine.photo}`} alt="Machine" />}
             <button onClick={startTimer}>Start</button>
             <button onClick={pauseTimer}>Pause</button>
             <button onClick={stopTimer}>Stop</button>
