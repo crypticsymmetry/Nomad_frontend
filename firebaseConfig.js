51add858a24e7604077ea08c322cd7346967daa1
crypticsymmetry <ccbb121@gmail.com>
2024-07-06 18:30:22 -0700

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,14 +2,15 @@ const firebase = require("firebase/app");
 require("firebase/firestore");
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
-};
-
+    apiKey: "AIzaSyCEjiw2NdpMSIuv0_VKXHzlrMywAyMtYg4",
+    authDomain: "nomadpowersports-84e70.firebaseapp.com",
+    projectId: "nomadpowersports-84e70",
+    storageBucket: "nomadpowersports-84e70.appspot.com",
+    messagingSenderId: "476619609513",
+    appId: "1:476619609513:web:15a6bcc9a8677a9e0ce0b8",
+    measurementId: "G-M1D1T3QZRX"
+  };
+  
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
