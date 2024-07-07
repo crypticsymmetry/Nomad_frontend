f1d26670f1e910d5c64c76300fc63116820c34c0
crypticsymmetry <ccbb121@gmail.com>
2024-07-07 16:58:39 -0700

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,21 +1,18 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+const firebase = require("firebase/app");
+require("firebase/firestore");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEjiw2NdpMSIuv0_VKXHzlrMywAyMtYg4",
-  authDomain: "nomadpowersports-84e70.firebaseapp.com",
-  projectId: "nomadpowersports-84e70",
-  storageBucket: "nomadpowersports-84e70.appspot.com",
-  messagingSenderId: "476619609513",
-  appId: "1:476619609513:web:15a6bcc9a8677a9e0ce0b8",
-  measurementId: "G-M1D1T3QZRX"
-};
+    apiKey: "AIzaSyCEjiw2NdpMSIuv0_VKXHzlrMywAyMtYg4",
+    authDomain: "nomadpowersports-84e70.firebaseapp.com",
+    projectId: "nomadpowersports-84e70",
+    storageBucket: "nomadpowersports-84e70.appspot.com",
+    messagingSenderId: "476619609513",
+    appId: "1:476619609513:web:15a6bcc9a8677a9e0ce0b8",
+    measurementId: "G-M1D1T3QZRX"
+  };
+  
+firebase.initializeApp(firebaseConfig);
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const storage = getStorage(app);
-const auth = getAuth(app);
+const db = firebase.firestore();
 
-export { db, storage, auth, signInWithEmailAndPassword, onAuthStateChanged };
+module.exports = db;
