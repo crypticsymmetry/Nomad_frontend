e701dbfd33829307d4ea7f353b5abdaf57c0cf21
crypticsymmetry <ccbb121@gmail.com>
2024-07-06 18:48:36 -0700

    Update Home.js firebases

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import db from '../firebaseConfig'; // Import Firestore
+import { db } from '../firebaseConfig'; // Use named import
 import { Link } from 'react-router-dom';
 import './Home.css';
 
