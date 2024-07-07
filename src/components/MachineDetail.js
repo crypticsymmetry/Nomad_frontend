e5bf0bfd809036124763bdf8983255973b5dea8e
crypticsymmetry <ccbb121@gmail.com>
2024-07-07 01:01:33 -0700

    Update MachineDetail.js

--- src/components/MachineDetail.js
+++ src/components/MachineDetail.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { db } from '../firebaseConfig'; // Use named import
+import { db } from '../firebaseConfig';
 import { useParams, Link } from 'react-router-dom';
 import Select from 'react-select';
 import axios from 'axios';
