97383748f0d62c0c93662fdb62e21485dd0e9ed0
crypticsymmetry <ccbb121@gmail.com>
2024-07-08 15:09:44 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import axios from 'axios';
+import { auth } from './firebaseConfig';
 
-// Set the base URL for Axios requests
-axios.defaults.baseURL = 'https://nomad-backend-1.onrender.com'; // Replace with your Render backend URL
+// Sign in the user anonymously
+auth.signInAnonymously().catch(error => {
+    console.error('Error signing in anonymously:', error);
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
